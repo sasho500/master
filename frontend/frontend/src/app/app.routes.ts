cc409c070877d08ffd6324dbd72010871513f43c
Aleksandar Palikov <72619653+sasho500@users.noreply.github.com>
2024-09-14 21:57:40 +0300

    Added default router

--- frontend/frontend/src/app/app.routes.ts
+++ frontend/frontend/src/app/app.routes.ts
@@ -17,6 +17,7 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./admin/admin.module').then((m) => m.AdminModule),
   },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 @NgModule({
   imports: [
